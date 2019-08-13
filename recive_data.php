636bea0c6ffb1452396c5f9e6e93e53ea5edf503
Mishan Madhupa <mishancool2016@gmail.com>
2019-08-13 17:18:15 +0530

    Create recive_data file which data insert  in to database

--- recive_data.php
+++ recive_data.php
@@ -1,13 +1,41 @@
 <?php
+//Creates new record as per request
+    //Connect to database
     $servername = "localhost";
     $username = "root";
     $password = "";
     $dbname = "iot_project";
-    
+ 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } 
+        die("Database Connection failed: " . $conn->connect_error);
+    }
+ 
+    //Get current date and time
+    date_default_timezone_set('Asia/Kolkata');
+    $d = date("Y-m-d");
+    //echo " Date:".$d."<BR>";
+    $t = date("H:i:s");
+ 
+    if(!empty($_GET['status']) && !empty($_GET['station']))
+    {
+        $speed = $_GET['speed'];
+        
+    	$bill = $_GET['bill'];
+ 
+	    $sql = "INSERT INTO logs (speed, bill, Date, Time)
+		
+		VALUES ('".$speed."', '".$bill."', '".$d."', '".$t."')";
+ 
+		if ($conn->query($sql) === TRUE) {
+		    echo "OK";
+		} else {
+		    echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+	}
+ 
+ 
+	$conn->close();
 ?>
