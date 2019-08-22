3f5d3fee5b8fb0d21587f8de5ba104b041d1ecbd
Mishan Madhupa <mishancool2016@gmail.com>
2019-08-22 10:49:59 +0530

    Code html tags, Connect to database and Set logo image

---
+++ monthly_usage.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    
+
+</head>
+<body>
+
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "iot_project";
+        
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+    ?>
+
+    <div class="table_background">
+
+        <div class="logo"> 
+            <img src="img/Logo_Design.png" alt="Logo" width="500" height="150" >
+        </div>
+
+    </div>
+
+    
+</body>
+</html>
