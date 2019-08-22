e48b361647407cc955c538638b375be7035a5c68
Mishan Madhupa <mishancool2016@gmail.com>
2019-08-22 23:57:35 +0530

    Code html tags and Connect to database

---
+++ yearly_usage.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" type="text/css" href="css/3_daily_usage.css">
+
+</head>
+<body>
+
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "iot_project";
+        
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+    ?>
+
+    
+</body>
+</html>
