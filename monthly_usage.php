ddf015dba6167f4f6fd4a35f83e0f559446da2e6
Mishan Madhupa <mishancool2016@gmail.com>
2019-08-24 22:58:45 +0530

    Add comment

--- monthly_usage.php
+++ monthly_usage.php
@@ -78,7 +78,7 @@
             $monthly_water_usage=0;
             $pay=0;
 
-            $sql = "SELECT speed, date FROM iot_water where MONTH(CURDATE())";
+            $sql = "SELECT speed, date FROM iot_water where MONTH(CURDATE())"; //Filter from month
             $result = $conn->query($sql);
 
             
@@ -105,7 +105,7 @@
 
                     echo '<h3>'.$time.'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$date.'<h3>'.'<br><br>';
 
-                    echo '<h2>Monthly whater usage = ' .$monthly_water_usage .' Leaters<h2>'.'<br>';
+                    echo '<h2>Monthly whater usage = ' .$monthly_water_usage .' Liters<h2>'.'<br>';
 
                     echo '<h2>Charge for water consumed = ' . $pay .' Rs<h2>'.'<br>';
 
